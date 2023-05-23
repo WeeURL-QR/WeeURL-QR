fdcc6d8a91b2c166ae8c032f4931c1b92db625ca
JakeGillan <126023939+JakeGillan@users.noreply.github.com>
2023-05-23 16:58:42 -0500

    resolved webpack errors

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from './assets/weelad.png'
+
 
 
 
@@ -7,9 +9,10 @@ class Navbar extends Component {
 	render() {
 		return(
 		<header className="navbar">
-			<Link to="/" className="logo">Wee-URL-QR</Link>
+			<Link to="/" className="logo">Wee<img id="logo" src={logo} alt="Logo2"/>URL-QR</Link>
 			{/* <img src={weelad}></img> */}
-			<img src="./assets/weelad.png" alt="Logo" />
+			{/* <img src="./assets/weelad.png" alt="Logo"></img> */}
+			{/* <img src={logo} alt="Logo2"/> */}
 
 			<nav>
 				{/* could add link to Site Visits here */}
