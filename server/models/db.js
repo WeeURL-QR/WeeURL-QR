7a3b7f5ff89a033d2f8e851e55b48235dcd88dbf
Cartlong7 <crlong7@gmail.com>
2023-05-23 11:12:02 -0600

    middleware for getUrl complete

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,7 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 // get PG uri from RDS
 const PG_URI = 'postgresql://urladmin:QjJ9ChR2DNfmvXNkWOHe@urldb.cmd5f7fuanao.us-west-2.rds.amazonaws.com:5432/postgres';
+
 // create a new pool using the connection string above
 const pool = new Pool({
     connectionString: PG_URI
@@ -11,9 +12,10 @@ const pool = new Pool({
 // This will be required in the controllers to be the access point to the database
 module.exports = {
     query: (text, params, callback) => {
-      console.log('executed query to DB', text);
+      console.log('executed query to DB');
+      // console.log('executed query to DB, this is the params', params);
+      // console.log('executed query to DB, this is the callback', callback);
     //   this pool.query is specifically to our database
       return pool.query(text, params, callback);
     }
   };
-
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,13 +6,14 @@ const router = express.Router();
 
 // have method to check if it exists in database
 // if returns false, then go to adding it
-router.post('/getURL' /* controller here */, (req, res) => {
-    res.status(200).json(res.locals)
+router.post('/getURL', urlController.getUrl, (req, res) => {
+    res.status(200).json(res.locals.shortUrl)
 })
 
 // get request for shortened to get long url
-router.get('/something', /* controller */ (req, res) => {
+router.get('/:?', urlController.shortToLong, (req, res) => {
     // relocate to new new long url in response. res.redirect?
+    res.status(200).redirect(res.locals.longUrl)
 })
 
 module.exports = router;
