7ff72dbade7e4ecb942217980c9485e2048bdc4a
Cartlong7 <crlong7@gmail.com>
2023-05-23 08:06:31 -0600

    updated server route hanlder and module imports

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,14 @@
 const express = require('express')
 // import in controller
-
+const urlController = require('../controller/urlController');
 // declare router variable
 const router = express.Router();
 
 // have method to check if it exists in database
 // if returns false, then go to adding it
-
 router.post('/getURL' /* controller here */, (req, res) => {
     res.status(200).json(res.locals)
 })
-
 
 // get request for shortened to get long url
 router.get('/something', /* controller */ (req, res) => {
--- server/server.js
+++ server/server.js
@@ -1,14 +1,9 @@
 // all router logic in a single file. 
 // route to here, middleware/controllers
-import { Express } from "express";
+const express = require('express');
 const path = require('path');
-// will need to require in database from db.js
-// DON"T think this path is correct, double check later
-const db = require(servers/db.js)
-
-
 const app = express();
-const router = express.Router();
+const apiRouter = require('./routes/api');
 const PORT = 3000;
 
 // handle parsing request body
@@ -18,8 +13,7 @@ app.use(express.urlencoded({ extended: true}));
 // handle requests for static files
 app.use(express.static(path.resolve(__dirname, '../client')));
 
-// router logic is implemented below, using middleware from controllers folder
-// catch all for api to send to api routes
+// route handler
 app.use('/api', apiRouter)
 
 // catch all route handling for requests to unknown routers
@@ -27,6 +21,7 @@ app.use((req,res) => {
   res.status(400).send('The server could not be reached, check that the url you have entered is correct.')
 });
 
+// express error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
@@ -38,6 +33,7 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
 });
